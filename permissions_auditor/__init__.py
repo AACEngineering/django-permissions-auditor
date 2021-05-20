25cd67d9584f2326b62d512de61e216ab60992a6
Christian Klus <christianklus@gmail.com>
2021-05-20 14:58:46 -0500

    Revert default_app_config change

--- permissions_auditor/__init__.py
+++ permissions_auditor/__init__.py
@@ -1,10 +1,6 @@
 "Permissions auditing for Django."
 
-import django
-
-
 __version__ = '1.0.4'
 
 
-if django.VERSION < (3, 2):
-    default_app_config = 'permissions_auditor.apps.PermissionsAuditorConfig'
+default_app_config = 'permissions_auditor.apps.PermissionsAuditorConfig'
