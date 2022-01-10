dd351fe1a82630b95913cd1fcc39008371c76f53
Christian Klus <christianklus@gmail.com>
2022-01-10 12:04:48 -0600

    Don't try to suppress default_app_config warning

--- permissions_auditor/__init__.py
+++ permissions_auditor/__init__.py
@@ -3,6 +3,4 @@
 __version__ = '1.0.5'
 
 # This can be removed once Django 3.1 and below support is dropped.
-from django import VERSION as DJANGO_VERSION
-if DJANGO_VERSION < (3, 1):
-    default_app_config = 'permissions_auditor.apps.PermissionsAuditorConfig'
+default_app_config = 'permissions_auditor.apps.PermissionsAuditorConfig'
